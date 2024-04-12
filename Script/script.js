437701d9eabec34a70bcfc720883ecf5f20a8c68
NurSultan56 <nurhuseynov56@gmail.com>
2024-04-12 13:36:36 +0400

    DONE

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,7 @@
 const taskForm = document.getElementById("task-form")
 const taskList = document.getElementById("task-list")
 
+// КНОПКА ДОБАВИТЬ
 taskForm.addEventListener('submit', (event) => {
     event.preventDefault()
 
@@ -20,6 +21,7 @@ taskForm.addEventListener('submit', (event) => {
     deleteTaskButton.setAttribute('type', 'button')
     taskItem.appendChild(deleteTaskButton)
 
+    // УДАЛИТЬ ЭЛЕМЕНТ СПИСКА
     deleteTaskButton = document.querySelectorAll("#list-delete-button")
     deleteTaskButton.forEach((item) => {
         item.addEventListener('click', () => {
@@ -28,36 +30,56 @@ taskForm.addEventListener('submit', (event) => {
     })
 })
 
-
+// КНОПКА УДАЛИТЬ ПЕРВЫЙ ЭЛЕМЕНТ
+var taskArr = []
 const inputDeleteButton = document.getElementById("input-delete-button")
-
 inputDeleteButton.addEventListener('click', () => {
     const taskInput = document.querySelectorAll("#task-input")
-    taskInput[0].value = ''
+    if (taskInput.length>1) {
+    taskInput.forEach((item)=>{
+        taskArr.push(item.value)
+    })
+    taskArr.splice(0,1)
+    for (let i=0; i<taskArr.length; i++){
+        taskInput[i].value = taskArr[i]
+    }
+    document.querySelector(".list-element-container:last-child").remove()
+    taskArr.splice(0,taskArr.length)
+    } else {
+        taskInput.forEach((item)=>{
+            item.value = ''
+        })
+    }
 });
 
-
+// КНОПКА СОРТИРОВКИ (ТУДА И ОБРАТНО)
 const sortButton = document.getElementById("sort-list")
-
-var taskArr = []
-
+var flag = true
 sortButton.addEventListener('click', () => {
-    let flag = true
     if (flag === true) {
         sortButton.className = 'list-sort-type-button-increase'
-
         const taskInput = document.querySelectorAll("#task-input")
         taskInput.forEach((item) => {
             taskArr.push(item.value)
         })
         taskArr.sort((a, b) => a.localeCompare(b))
+        for (let i=0; i<taskArr.length; i++){
+            taskInput[i].value = taskArr[i]
+        }
         flag = false
-        console.log(taskArr)
+        taskArr.splice(0,taskArr.length)
 
     } else {
         sortButton.className = 'list-sort-type-button-decrease'
+        const taskInput = document.querySelectorAll("#task-input")
+        taskInput.forEach((item) => {
+            taskArr.push(item.value)
+        })
         taskArr.sort((a, b) => b.localeCompare(a))
+        for (let i=0; i<taskArr.length; i++){
+            taskInput[i].value = taskArr[i]
+        }
         flag = true
-        console.log(taskArr)
+        taskArr.splice(0,taskArr.length)
     }
 })
